103aa3cef81727ab337ac86c8178c762b4ffec26
Dmitry <karpovichdi@hotmail.com>
2018-11-03 18:38:21 +0300

    setting gradle

---
+++ app/src/main/java/comdfsgfrgtdg/example/karpo/task/MainActivity.java
@@ -0,0 +1,13 @@
+package comdfsgfrgtdg.example.karpo.task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
